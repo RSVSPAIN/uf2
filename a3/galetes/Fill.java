2ee4a8e9b1d6ff1e1b82495f4381c0c7d7332822
jherna <jherna76@elpuig.xeill.net>
2018-01-12 16:43:27 +0100

    pot de galetes

---
+++ a3/galetes/Fill.java
@@ -0,0 +1,32 @@
+package a3.galetes;
+
+
+public class Fill implements Runnable  {
+	private String Nom;
+	private Familia fa;
+
+	public Fill(Familia f, String nom) {
+		fa=f;
+		Nom = nom;
+	} 
+	
+	public Fill() {
+		super();
+	}
+
+	public Fill(String nom) {
+		super();
+		Nom = nom;
+	}
+	
+
+	@Override
+	public void run() {
+		if((fa.agafaGaletes(1))==0) fa.setNoHiHaGaletes();
+		System.out.println("Sóc " + Nom + " galetes que queden = " + fa.getGaletes());
+		
+	}
+	
+		
+
+}
